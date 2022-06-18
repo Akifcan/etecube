e697225c349c86fc3a13611cd66b2c52ac8c1668
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-06-18 12:22:20 +0300

    search company

--- frontend/pages/company/index.tsx
+++ frontend/pages/company/index.tsx
@@ -1,15 +1,19 @@
 import { FC, useState, useEffect } from 'react'
 import Container from '@/components/Container'
-import { Table, Button, Pagination } from 'antd'
+import { Table, Button, Pagination, Input, Row, Select, Col } from 'antd'
 import Head from 'next/head'
 import Link from 'next/link'
 import http from '@/helpers/http'
 import { CompanyProps } from '@/interfaces/company'
+const { Search } = Input
+const { Option } = Select
 
 const Company: FC = () => {
 
     const [currentPage, setCurrentPage] = useState(1)
+    const [search, setSearch] = useState('')
     const [totalPage, setTotalPage] = useState<number>()
+    const [order, setOrder] = useState('ASC')
 
     const [companies, setCompanies] = useState<CompanyProps[]>([])
 
@@ -37,7 +41,7 @@ const Company: FC = () => {
     ]
 
     const loadCompanies = async () => {
-        const companies = await http<{ total: number, companies: CompanyProps[] }>(`/company?page=${currentPage}`, 'GET')
+        const companies = await http<{ total: number, companies: CompanyProps[] }>(`/company?page=${currentPage}&name=${search}&order=${order}`, 'GET')
         setCompanies(companies.data.companies.map((company) => {
             return { ...company, key: company.id }
         }))
@@ -47,13 +51,29 @@ const Company: FC = () => {
     useEffect(() => {
         loadCompanies()
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currentPage])
+    }, [currentPage, search, order])
+
+    const onSearch = (value: string) => {
+        setSearch(value)
+        setCurrentPage(1)
+    }
 
     return <>
         <Head>
             <title>Companies</title>
         </Head>
         <Container header={{ title: 'Companies', subtitle: 'Manage Companies' }}>
+            <Row>
+                <Col flex={1}>
+                    <Search placeholder="input search text" allowClear onSearch={onSearch} style={{ width: '100%', marginBlockEnd: '1rem' }} />
+                </Col>
+                <Col>
+                    <Select value={order} onChange={(e) => setOrder(e)}>
+                        <Option value='ASC'>A-Z</Option>
+                        <Option value='DESC'>Z-A</Option>
+                    </Select>
+                </Col>
+            </Row>
             <Link href={'/company/add'} passHref={true}>
                 <Button type='dashed' style={{ marginBlockEnd: '1rem' }}>Add New Company</Button>
             </Link>
